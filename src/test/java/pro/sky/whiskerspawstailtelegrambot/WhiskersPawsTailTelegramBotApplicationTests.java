9da6221f24e84d4adc697e1d2d04cb3385576df8
AntonGraf <ant0ngraf@yandex.ru>
2022-12-23 10:51:30 +0600

    Initial commit

---
+++ src/test/java/pro/sky/whiskerspawstailtelegrambot/WhiskersPawsTailTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.whiskerspawstailtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskersPawsTailTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
