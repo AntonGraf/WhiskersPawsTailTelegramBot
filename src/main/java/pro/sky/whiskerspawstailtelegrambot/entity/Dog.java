d95c3a144f78c1a850e59e33299575087673a847
lupaev <lupaevsv@gmail.com>
2022-12-30 02:36:49 +0700

    сущность собаки создана, таблица создается корректно

--- src/main/java/pro/sky/whiskerspawstailtelegrambot/entity/Dog.java
+++ src/main/java/pro/sky/whiskerspawstailtelegrambot/entity/Dog.java
@@ -3,10 +3,7 @@ package pro.sky.whiskerspawstailtelegrambot.entity;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -16,11 +13,21 @@ import javax.persistence.Id;
 @Entity
 public class Dog {
     @Id
-    @GeneratedValue
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+
+    @Column (name = "name")
     String fullName;
+
+    /**
+     * При создании таблицы будет установлено ограничение: age > 0
+     */
     int age;
+
+    /**
+     * Необходимо установить ограничение на размер файла при заполнении БД. Например в 1024Кб.
+     * Продублировать это ограничение в контроллер для Swagger API для заполнения БД.
+     */
     Byte[] photo;
     String description;
 }
--- src/main/resources/db.changelog/v-1.0/01-create-dog-table.sql
+++ src/main/resources/db.changelog/v-1.0/01-create-dog-table.sql
@@ -1,16 +1,10 @@
 CREATE TABLE dog
 (
     id BIGINT PRIMARY KEY,
-    fullName TEXT,
+    name TEXT,
     age INTEGER CHECK ( age > 0 ),
     description TEXT,
     photo bytea
 );
 
-CREATE TABLE photo (
-    data bytea
-);
 
-ALTER TABLE photo ALTER COLUMN data SET STORAGE EXTERNAL;
-
-INSERT INTO photo VALUES ( pg_read_binary_file ('/Users/sergej/Downloads/dog1.jpeg')::bytea);
