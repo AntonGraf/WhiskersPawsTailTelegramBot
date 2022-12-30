ec805e3efc87f0b09b53b8e71a2b9be14deccb98
AntonGraf <ant0ngraf@yandex.ru>
2022-12-30 11:29:56 +0600

    Добавляет комментарии для JavaDoc

--- src/main/java/pro/sky/whiskerspawstailtelegrambot/entity/Shelter.java
+++ src/main/java/pro/sky/whiskerspawstailtelegrambot/entity/Shelter.java
@@ -6,6 +6,9 @@ import lombok.experimental.FieldDefaults;
 import javax.persistence.*;
 import java.util.List;
 
+/**
+ * Используется для хранения информации о приютах:
+ */
 @Getter
 @Setter
 @ToString
@@ -16,23 +19,34 @@ import java.util.List;
 public class Shelter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
+    //Информация о приюте
     String aboutShelter;
+    //Информация о технике безопасности
     String shelterSafetyEquipment;
+    //Правила знакомства с собакой
     String ruleOfMeeting;
+    //Список документов, необходимых для того, чтобы взять собаку из приюта
     @ElementCollection
     @CollectionTable(name = "shelter_list_of_doc", joinColumns = @JoinColumn(name = "shelter_id"))
     @Column(name = "list_if_doc")
     List<String> listOfDoc;
+    //Рекомендации по транспортировке животного
     String recOfTransportation;
+    //Рекомендации по обустройству дома для щенка
     String homeImprovementForPuppy;
+    //Рекомендации по обустройству дома для взрослой собаки
     String homeImprovementForDog;
+    //Рекомендации по обустройству дома для взрослой собаки с ограниченными возможностями
     String homeImprovementForDogWithDisabilities;
+    //Советы кинолога по первичному общению с собакой
     String cynologistAdvice;
+    //Список рекомендуемых приютом кинологов
     @ElementCollection
     @CollectionTable(name = "shelter_cynologist", joinColumns = @JoinColumn(name = "shelter_id"))
     @Column(name = "cynologist")
     List<String> cynologist;
+    //Список причин, по которым могут отказать и не дать забрать собаку
     @ElementCollection
     @CollectionTable(name = "shelter_list_of_reason_for_rejection", joinColumns = @JoinColumn(name = "shelter_id"))
     @Column(name = "list_of_reason_for_rejection")
