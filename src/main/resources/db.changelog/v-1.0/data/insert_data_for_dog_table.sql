be346db395f7230005e04ba5817592e3dd191a6d
lupaev <lupaevsv@gmail.com>
2023-01-05 00:03:25 +0700

    Добавлены тестовые данные в таблицу Собака

---
+++ src/main/resources/db.changelog/v-1.0/data/insert_data_for_dog_table.sql
@@ -0,0 +1,4 @@
+insert into dog (id, name, age, description, shelter_id) values
+                                                             (1, 'Бобик', 5, 'хороший', 1),
+                                                             (2, 'Шарик', 6, 'плохой', 1),
+                                                             (3, 'Бим', 7, 'злой', 1);
