4e18535514c83a2e6670d69a527312709b8ac902
igr76 <gritsuk76@mail.ru>
2023-01-05 20:06:33 +1000

    заполнение таблицы волонтеров

--- src/main/resources/db.changelog/v-1.0/04_great_table_volunteer.sql
+++ src/main/resources/db.changelog/v-1.0/04_great_table_volunteer.sql
@@ -1,14 +1,10 @@
-CREATE TABLE volunteer_info
-(
-    id  bigserial ,
-    info_volunteer TEXT
 
-)
 CREATE TABLE volunteer
 (
     id  bigserial PRIMARY KEY ,
+    info_volunteer TEXT,
     full_name VARCHAR(256) ,
     phone VARCHAR(30),
     schedule TEXT
 
-)
+);
