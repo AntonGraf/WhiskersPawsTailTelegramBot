c75750cb08da02d148413ae275e8e4215f8e1624
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-01-05 19:35:32 +0600

    исправил названия текстовых констант

--- src/main/java/pro/sky/whiskerspawstailtelegrambot/TextAndButtons/AllText.java
+++ src/main/java/pro/sky/whiskerspawstailtelegrambot/TextAndButtons/AllText.java
@@ -2,8 +2,8 @@ package pro.sky.whiskerspawstailtelegrambot.TextAndButtons;
 
 public abstract class AllText {
 
-  public static final String startText = "/start";
-  public static final String infoShelterText = "Info дописать текст";
+  public static final String START_TEXT = "/start";
+  public static final String INFO_SHELTER_TEXT = "Info дописать текст";
 
 
 
