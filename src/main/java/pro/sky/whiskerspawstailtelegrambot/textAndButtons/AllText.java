a0d1c44089078077d7604247d583a41089aad758
dmitr <dmitriypospelov93@gmail.com>
2023-01-06 12:44:01 +0700

    Handler

--- src/main/java/pro/sky/whiskerspawstailtelegrambot/textAndButtons/AllText.java
+++ src/main/java/pro/sky/whiskerspawstailtelegrambot/textAndButtons/AllText.java
@@ -4,7 +4,7 @@ public abstract class AllText {
 
   public static final String START_TEXT = "/start";
 
-  public static final String CALL_TO_VOLUNTEER = "/callToVolunteer ";
+  public static final String CALL_TO_VOLUNTEER = "/calltovolunteer";
 
   public static final String INFO_SHELTER_TEXT = "Info дописать текст";
 
