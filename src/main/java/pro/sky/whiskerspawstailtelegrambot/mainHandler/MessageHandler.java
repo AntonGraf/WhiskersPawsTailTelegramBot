2007ca9b20fc02ef40ab261725d293c456893743
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-01-10 02:47:57 +0600

    Добавил текст кнопок, кнопки, изменил способ формирования клавиатуры, обернул в try catch MessageHander, сделал  ReportService, сделал parserDog

--- src/main/java/pro/sky/whiskerspawstailtelegrambot/mainHandler/MessageHandler.java
+++ src/main/java/pro/sky/whiskerspawstailtelegrambot/mainHandler/MessageHandler.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import pro.sky.whiskerspawstailtelegrambot.service.ReportService;
 import pro.sky.whiskerspawstailtelegrambot.service.ShelterService;
 import pro.sky.whiskerspawstailtelegrambot.service.VolunteerService;
 import pro.sky.whiskerspawstailtelegrambot.textAndButtonsAndKeyboard.ConfigKeyboard;
@@ -29,16 +30,19 @@ public class MessageHandler implements MainHandler {
 
   private final ParserToBot parserToBot;
   private final ShelterService shelterService;
+  private final ReportService reportService;
 
 
   public MessageHandler(ConfigKeyboard configKeyboard, ReportAddHandler reportAddHandler,
-                        FormReplyMessages formReplyMessages, VolunteerService volunteerService, ParserToBot parserToBot, ShelterService shelterService) {
+      FormReplyMessages formReplyMessages, VolunteerService volunteerService,
+      ParserToBot parserToBot, ShelterService shelterService, ReportService reportService) {
     this.configKeyboard = configKeyboard;
     this.reportAddHandler = reportAddHandler;
     this.formReplyMessages = formReplyMessages;
     this.volunteerService = volunteerService;
     this.parserToBot = parserToBot;
     this.shelterService = shelterService;
+    this.reportService = reportService;
   }
 
   /**
@@ -49,54 +53,78 @@ public class MessageHandler implements MainHandler {
    */
   @Override
   public SendMessage handler(Update update) {
-    SendMessage sendMessage = null;
+
     String chatId = String.valueOf(update.getMessage().getChatId());
-    boolean checkUpdate = !update.getMessage().hasText();
-    if (!checkUpdate) {
-      log.debug("Обработка сообщения в виде текста");
-      Message message = update.getMessage();
-      String textMessage = message.getText();
-      //здесь инжект текст кнопок, любой текст крч
-      switch (textMessage) {
+    SendMessage sendMessage = new SendMessage(chatId, AllText.ERROR_REPLY_TEXT);
+    try {
+      boolean checkUpdate = !update.getMessage().hasText();
+      if (!checkUpdate) {
+        log.debug("Обработка сообщения в виде текста");
+        Message message = update.getMessage();
+        String textMessage = message.getText();
+        //здесь инжект текст кнопок, любой текст крч
+        switch (textMessage) {
 
-        case (AllText.START_TEXT):
-          sendMessage = formReplyMessages.replyMessage(message, AllText.WELCOME_MESSAGE_TEXT,
-              configKeyboard.initKeyboardOnClickStart());
-          break;
+          case (AllText.START_TEXT):
+            sendMessage = formReplyMessages.replyMessage(message, AllText.WELCOME_MESSAGE_TEXT,
+                configKeyboard.initKeyboardOnClickStart());
+            break;
 
-        case (AllText.CALL_TO_VOLUNTEER_TEXT): //ответ на позвать волонтера, просто инфа про волонтеров
-          sendMessage = formReplyMessages.replyMessage(message, parserToBot.parserVolunteer(volunteerService.getAllVolunteers()),
-                  configKeyboard.initKeyboardOnClickStart());
-          break;
+          case (AllText.CANCEL_TEXT)://реакция на кнопку отмена - возврат в главное меню
+            sendMessage = formReplyMessages.replyMessage(message,
+                AllText.CANCEL_RETURN_MAIN_MENU_TEXT,
+                configKeyboard.initKeyboardOnClickStart());
+            break;
 
-        case (AllText.SEND_PET_REPORT_TEXT):     // реализация логики отправить отчет
-          sendMessage = reportAddHandler.handlerReport(message, AllText.MENU_SEND_PET_REPORT_TEXT,
-              configKeyboard.initKeyboardOnClickSendPetReport());
-          break;
-        case (AllText.HOW_TAKE_DOG):
-          sendMessage = formReplyMessages.replyMessage(message, AllText.HOW_TAKE_DOG_SHELTER,
-                  configKeyboard.initKeyboardOnClickStart());
-          break;
-        case (AllText.INFO_SHELTER_TEXT):
-                 sendMessage = formReplyMessages.replyMessage(message, shelterService.getOfShelterMessage(1L),
-                  configKeyboard.initKeyboardOnClickStart());
-          break;
+          case (AllText.CALL_TO_VOLUNTEER_TEXT): //ответ на позвать волонтера, просто инфа про волонтеров
+            sendMessage = formReplyMessages.replyMessage(message,
+                parserToBot.parserVolunteer(volunteerService.getAllVolunteers()),
+                configKeyboard.initKeyboardOnClickStart());
+            break;
 
+          //region реализация логики Отправить отчет о питомце
+          case (AllText.SEND_PET_REPORT_TEXT):     // нажатие кнопки Отправить отчет о питомце
+            sendMessage = reportAddHandler.getSendMessageReport(message, AllText.MENU_SEND_PET_REPORT_TEXT,
+                configKeyboard.formReplyKeyboardInOneRow(AllText.SHOW_ALL_YOUR_PET_TEXT, AllText.SEND_REPORT_TEXT, AllText.CANCEL_TEXT));
+            break;
 
-        default:
-          sendMessage = new SendMessage(chatId, AllText.UNKNOWN_COMMAND_TEXT);
-          break;
+          case (AllText.SHOW_ALL_YOUR_PET_TEXT):     // нажатие кнопки показать всех взятых животных
+            sendMessage = reportAddHandler.getSendMessageReport(message,
+                reportService.showAllAdoptedPets(message),
+                configKeyboard.formReplyKeyboardInOneRow(AllText.SHOW_ALL_YOUR_PET_TEXT, AllText.SEND_REPORT_TEXT, AllText.CANCEL_TEXT));
+            break;
+
+          case (AllText.SEND_REPORT_TEXT):     // нажатие кнопки отправить отчет
+            sendMessage = reportAddHandler.sendReport(message, AllText.DESCRIPTION_SEND_REPORT_TEXT,
+                configKeyboard.formReplyKeyboardInOneRow(AllText.SEND_TEXT, AllText.CANCEL_TEXT));
+            break;
+          //endregion
+
+          case (AllText.HOW_TAKE_DOG):
+            sendMessage = formReplyMessages.replyMessage(message, AllText.HOW_TAKE_DOG_SHELTER,
+                configKeyboard.initKeyboardOnClickStart());
+            break;
+          case (AllText.INFO_SHELTER_TEXT):
+            sendMessage = formReplyMessages.replyMessage(message,
+                shelterService.getOfShelterMessage(1L),
+                configKeyboard.initKeyboardOnClickStart());
+            break;
+
+          default:
+            sendMessage = new SendMessage(chatId, AllText.UNKNOWN_COMMAND_TEXT);
+            break;
+        }
       }
+    } finally {
+      return sendMessage;
     }
-    return sendMessage;
   }
-
-  //для удобства
-  private String readUpdate(Update update) {
-    return update.getMessage().getText();
-  }
-
 
 }
+
+
+
+
+
 
 
