6114c4efdf1defcd8d0587a07b21f8dc4aa835b1
sergej <lupaevsv@gmail.com>
2023-01-09 23:56:10 +0700

    Тестирование для DogService

---
+++ src/test/java/pro/sky/whiskerspawstailtelegrambot/service/DogServiceTest.java
@@ -0,0 +1,55 @@
+package pro.sky.whiskerspawstailtelegrambot.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pro.sky.whiskerspawstailtelegrambot.repository.DogRepository;
+
+class DogServiceTest {
+
+    private final long INT = 1;
+
+    DogRepository dogRepository;
+    DogService dogService;
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+
+    @Test
+    void findDog() {
+
+    }
+
+    @Test
+    void findAllDog() {
+    }
+
+    @Test
+    void removeDog() {
+    }
+
+    @Test
+    void editDog() {
+    }
+
+    @Test
+    void addIdAdoptiveParent() {
+    }
+
+    @Test
+    void uploadPhoto() {
+    }
+
+    @Test
+    void addDog() {
+    }
+}
