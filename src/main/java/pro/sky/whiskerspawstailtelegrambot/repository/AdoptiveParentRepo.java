1fc7bf9252edf2d1e9f2ee7cf5e487187a863bd4
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-01-13 12:38:05 +0600

    удалил лишнее

--- src/main/java/pro/sky/whiskerspawstailtelegrambot/repository/AdoptiveParentRepo.java
+++ src/main/java/pro/sky/whiskerspawstailtelegrambot/repository/AdoptiveParentRepo.java
@@ -12,10 +12,6 @@ public interface AdoptiveParentRepo extends JpaRepository<AdoptiveParent, Long> 
     AdoptiveParent getAdoptiveParentByPhone(String phone);
     AdoptiveParent getAdoptiveParentByChatId(Long chatId);
 
-//    @Query(value = "SELECT state FROM adoptive_parent where :chatId == adoptive_parent.chat_id", nativeQuery = true)
-//    AdoptiveParent getStateAdoptiveParentByChatId2(Long chatId);
-//    @Query(value = "SELECT * FROM adoptive_parent where :chatId == adoptive_parent.chat_id", nativeQuery = true)
-//    AdoptiveParent getAdoptiveParentByChatId2(@Param("chatId") Long chatId);
 
 
 
